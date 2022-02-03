8197f773897828ce2a84d37024bc570a6d9f1efd
Rohit <rohitkumbhar3456@gmail.com>
2022-02-03 23:37:53 +0530

    added some functionality

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import GridItem from "./components/GridItem";
+import GridRow from "./components/GridRow";
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+        boardState : [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""]
+        ],
+        turn:"X"
+    }
+  }
+
+  updateBoard=(row,col) =>{
+    const arr = this.state.boardState;
+    arr[row][col] = this.state.turn; 
+    this.setState({
+      turn:this.state.turn === "X" ? "0" : "X", 
+      boardState: arr
+    })
+  }
+
+  render(){
+    return(
+      <>
+        <div className="container">
+          {[0,1,2].map((row)=>{
+            return <GridRow index={row} boardState={this.state.boardState} updateBoard={this.updateBoard}/>
+          })}
+        </div>
+      </>
+    )
+  }
+}
+
+export default App;
